542f5afe9bf4302878f6b062cd7a93b124601af4
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-03-30 12:58:23 -0600

    Finalización del componente LazyImage

--- src/app/shared/lazyImage/lazyImage.component.ts
+++ src/app/shared/lazyImage/lazyImage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
@@ -6,9 +6,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LazyImageComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  public url!: string;
 
-  ngOnInit() {
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if( !this.url ) throw Error('URL property is required');
+  }
+
+  onLoad(){
+    console.log('Image loaded');
+    this.hasLoaded = true;
   }
 
 }
